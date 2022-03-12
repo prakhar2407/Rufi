d5317c1cc1d70152893fd64b6761a50342d8f2f1
Prakhar Bansal <prakhar19csu215@ncuindia.edu>
2022-03-12 17:24:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/rufi_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rufi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rufi_flutter/screens/Login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+  ));
+}
