405944e7efcbe83f5ca1aae8710920dc42531c8b
Prince016 <prashantsharma3854@gmail.com>
2022-03-12 19:08:58 +0530

    first commit edit singup

---
+++ lib/screens/SignUP.dart
@@ -0,0 +1,13 @@
+class Signup extends StatefulWidget {
+  Signup({Key? key}) : super(key: key);
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
