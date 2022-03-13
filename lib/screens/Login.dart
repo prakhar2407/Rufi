db15b1a057ea8210eaa8edbf63b5b25bd6a99b01
Prakhar Bansal <prakhar19csu215@ncuindia.edu>
2022-03-13 19:44:36 +0530

    Login page completed

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatelessWidget {
@@ -11,19 +12,18 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     double scHeight = MediaQuery.of(context).size.height;
     double scWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50, bottom: 50),
-        child: SingleChildScrollView(
-          child: Column(children: [
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SvgPicture.asset("assets/images/Login.svg",
-                    width: scWidth * 0.90, height: scHeight * 0.30),
+                SvgPicture.asset("assets/images/Login2.svg",
+                    width: scWidth * 0.8),
               ],
             ),
-            SizedBox(height: scHeight * 0.05),
+            SizedBox(height: scHeight * 0.01),
             Row(
               children: [
                 Padding(
@@ -38,10 +38,11 @@ class Login extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: scHeight * 0.05),
+            SizedBox(height: scHeight * 0.01),
             Padding(
               padding: const EdgeInsets.only(left: 30, right: 30),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
@@ -53,20 +54,11 @@ class Login extends StatelessWidget {
                   ),
                   TextFormField(
                     decoration: InputDecoration(
-                        // suffix: TextButton(
-                        //   style: ButtonStyle(
-                        //     padding: MaterialStateProperty.all(
-                        //       EdgeInsets.all(0),
-                        //     ),
-                        //   ),
-                        //   onPressed: () {},
-                        //   child: Text("Forgot?",
-                        //       style: GoogleFonts.ubuntu(
-                        //           textStyle: TextStyle(
-                        //               fontSize: 20,
-                        //               fontWeight: FontWeight.w500,
-                        //               color: Colors.blue))),
-                        // ),
+                        suffixStyle: GoogleFonts.ubuntu(
+                            textStyle: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold)),
                         labelStyle: GoogleFonts.ubuntu(
                             textStyle:
                                 TextStyle(fontSize: 20, color: Colors.grey)),
@@ -76,7 +68,152 @@ class Login extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: scHeight * 0.05),
+            SizedBox(height: scHeight * 0.02),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Forgot Password?",
+                        style: GoogleFonts.ubuntu(
+                            textStyle: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: scHeight * 0.02),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                    fixedSize: MaterialStateProperty.all(
+                        Size.fromWidth(scWidth * 0.85)),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: GoogleFonts.ubuntu(
+                        textStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: scHeight * 0.02),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 1.0,
+                  width: 150.0,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  width: scWidth * 0.02,
+                ),
+                Text(
+                  "OR",
+                  style: GoogleFonts.ubuntu(
+                      textStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  )),
+                ),
+                SizedBox(
+                  width: scWidth * 0.02,
+                ),
+                Container(
+                  height: 1.0,
+                  width: 150.0,
+                  color: Colors.grey,
+                )
+              ],
+            ),
+            SizedBox(
+              height: scWidth * 0.02,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color.fromRGBO(0, 0, 0, 0.1)),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                    fixedSize: MaterialStateProperty.all(
+                        Size.fromWidth(scWidth * 0.85)),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          ImageIcon(
+                            AssetImage("assets/images/google.png"),
+                            color: Color.fromARGB(255, 225, 62, 49),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Login with Google",
+                              style: GoogleFonts.ubuntu(
+                                  textStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 75, 71, 71))),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: scHeight * 0.02,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Wrap(
+                  children: [
+                    Text(
+                      "New to Rufi?  ",
+                      style: GoogleFonts.ubuntu(),
+                    ),
+                    InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Register",
+                          style: GoogleFonts.ubuntu(
+                              textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue)),
+                        ))
+                  ],
+                )
+              ],
+            )
           ]),
         ),
       ),
